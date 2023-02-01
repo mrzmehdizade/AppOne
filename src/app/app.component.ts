ccf871b67c211adae767fb44005ed61d09d523d4
marziye mehdizade <mrzmehdizade@gmail.com>
2023-02-01 12:08:43 +0330

    commit all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AppOne';
+
+  posts=[
+    {
+      title: "tree",
+      imageUrl:'assets/images/tree.jpeg',
+      userName:'nature',
+      content:'i saw neat tree'
+
+    },
+    {
+      title: "mountain",
+      imageUrl:'assets/images/mountain.jpeg',
+      userName:'mountain',
+      content:'i saw neat mountain'
+
+    },
+    {
+      title: "biking",
+      imageUrl:'assets/images/biking.jpeg',
+      userName:'biking',
+      content:'i saw neat biking'
+
+    },
+
+  ]
+
+
 }
